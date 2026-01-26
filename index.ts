87f4b22da674c2b2e8fb805ff6dfb7c6779b73f0
muuim <yumito.no.1@gmail.com>
2026-01-26 02:31:04 +0000

    good

--- index.ts
+++ index.ts
@@ -1,35 +1,39 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import { terminal } from 'terminal-kit';
-import path from 'path';
-import fs from 'fs';
+import { Command } from "commander";
+import { terminal } from "terminal-kit";
+import path from "path";
+import fs from "fs";
 
 function animateFrames(frames: string[], frameDelay = 80) {
-    let i = 0;
-    const interval = setInterval(() => {
-        terminal.clear();
-        terminal(frames[i]);
-        i = (i + 1) % frames.length;
-    }, frameDelay);
+  let i = 0;
 
-    process.on('SIGINT', () => {
-        clearInterval(interval);
-        terminal.clear();
-        process.exit();
-    });
+  terminal.clear();
+  terminal.wrap(false);
+
+  const interval = setInterval(() => {
+    terminal.wrap(false);
+    terminal.moveTo(1, 1);
+
+    terminal(frames[i]);
+    i = (i + 1) % frames.length;
+  }, frameDelay);
+
+  process.on("SIGINT", () => {
+    clearInterval(interval);
+    terminal.wrap(true);
+    terminal.clear();
+    process.exit();
+  });
 }
 
 const program = new Command();
 
-program
-  .name('memecli')
-  .description('meme on the terminal')
-  .version('1.0.0');
+program.name("memecli").description("meme on the terminal").version("1.0.0");
 
 program
-  .argument('<animation>', 'Name of the animation to run')
+  .argument("<animation>", "Name of the animation to run")
   .action((animation: string) => {
-    const animationPath = path.resolve(__dirname, 'meme', `${animation}.js`);
+    const animationPath = path.resolve(__dirname, "meme", `${animation}.js`);
 
     if (!fs.existsSync(animationPath)) {
       console.error(`Animation "${animation}" not found!`);
